06b162c9a8326421d1715e74c72d235d77e97a56
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-26 15:07:20 +0530

    Using Lombok Library To Auto Generate Setter And Getter For The DTO

--- src/main/java/com/example/emppayrollapp/DTO/EmpPayrollDTO.java
+++ src/main/java/com/example/emppayrollapp/DTO/EmpPayrollDTO.java
@@ -13,17 +13,8 @@ public class EmpPayrollDTO {
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public long getSalary() {
         return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
     }
 
     @Override
