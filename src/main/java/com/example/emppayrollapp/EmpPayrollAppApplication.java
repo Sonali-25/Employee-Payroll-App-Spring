c32239e5e0b05a82f87d29c245c0ef1cdfcab8b0
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-26 15:41:24 +0530

    Using Lombok Library For Logging

--- src/main/java/com/example/emppayrollapp/EmpPayrollAppApplication.java
+++ src/main/java/com/example/emppayrollapp/EmpPayrollAppApplication.java
@@ -1,13 +1,16 @@
 package com.example.emppayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmpPayrollAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmpPayrollAppApplication.class, args);
+        log.info("Employee Payroll App Started");
     }
 
 }
