b6835e30b4ae073d8bb9c827f9ad327d3462d894
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-27 20:50:33 +0530

    Add Remaining Properties To The DTO And Model

--- src/main/java/com/example/emppayrollapp/DTO/EmpPayrollDTO.java
+++ src/main/java/com/example/emppayrollapp/DTO/EmpPayrollDTO.java
@@ -1,35 +1,76 @@
 package com.example.emppayrollapp.DTO;
 
+import lombok.ToString;
+
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
-public class EmpPayrollDTO {
+public  @ToString class EmpPayrollDTO {
 
-    @NotEmpty(message = "Employee name cannot be null")
-    @Pattern(regexp = "^[A-Z] {1} [a-zA-z\\s] {2,}$", message = "Employee name Invalid")
+
+    @Pattern(regexp = "^[A-Z] {1} [a-zA-z\\s] {2,}$",
+            message = "Employee name Invalid")
     public String name;
 
-    @Min(value = 500, message = "Min wage should be more than 500")
+    @Min(value = 500,
+            message = "Min wage should be more than 500")
     public long salary;
+    public String gender;
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    public List<String> department;
 
-    public EmpPayrollDTO(String name, long salary){
-        this.name=name;
-        this.salary=salary;
+
+    public EmpPayrollDTO(String name, long salary, String gender,
+                         LocalDate startDate, String note, String profilePic, List<String> department) {
+        this.name = name;
+        this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
+        this.note = note;
+        this.profilePic = profilePic;
+        this.department = department;
 
     }
+
     public String getName() {
         return name;
     }
+
     public long getSalary() {
         return salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+    public String getNote() {
+        return note;
+    }
+    public String getProfilePic() {
+        return profilePic;
+    }
+    public List<String> getDepartment() {
+        return department;
     }
 
     @Override
     public String toString() {
         return "EmployeePayrollDto{" +
                 "name='" + name + '\'' +
-                ", salary=" + salary +
+                ", salary=" + salary + '\'' +
+                ", gender=" + gender + '\'' +
+                ", startDate=" + startDate + '\'' +
+                ", note=" + note + '\'' +
+                ", profilePic=" + profilePic + '\'' +
+                ", department=" + department + '\'' +
                 '}';
     }
 }
--- src/main/java/com/example/emppayrollapp/entity/EmployeePayrollData.java
+++ src/main/java/com/example/emppayrollapp/entity/EmployeePayrollData.java
@@ -1,39 +1,38 @@
 package com.example.emppayrollapp.entity;
 
 import com.example.emppayrollapp.DTO.EmpPayrollDTO;
+import lombok.Data;
 
-public class EmployeePayrollData {
+import java.time.LocalDate;
+import java.util.List;
+
+public @Data class EmployeePayrollData {
     private int employeeId;
     private String name;
     private long salary;
-
-    public EmployeePayrollData() {
-    }
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> department;
 
     public EmployeePayrollData(int employeeId, EmpPayrollDTO employeePayrollDTO) {
         this.employeeId = employeeId;
         this.name = employeePayrollDTO.getName();
         this.salary = employeePayrollDTO.getSalary();
+        this.gender = employeePayrollDTO.getGender();
+        this.startDate = employeePayrollDTO.getStartDate();
+        this.note =employeePayrollDTO.getNote();
+        this.profilePic = employeePayrollDTO.getProfilePic();
+        this.department = employeePayrollDTO.getDepartment();
     }
 
     public int getEmployeeId() {
         return employeeId;
     }
 
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public long getSalary() {
-        return salary;
     }
 
     public void setSalary(long salary) {
